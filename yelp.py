aa38a93c5009e4aef2f382edb2341c6031407b86
Andrew Peng <apeng@berkeley.edu>
2017-02-18 22:28:29 -0800

    first commit

--- yelp.py
+++ yelp.py
@@ -15,12 +15,12 @@ files = ["yelp_academic_dataset_business.json",
 
 file = open("yelp_academic_dataset_checkin.json", "r")
     #Load data
-business = [json.loads(line) for line in open(files[0], "r")]
-checkin = [json.loads(line) for line in open(files[1], "r")]
-review = [json.loads(line) for line in open(files[2], "r")]
-user = [json.loads(line) for line in open(files[3], "r")]
+business = [json.loads(line) for line in open(files[0], "r", encoding = 'utf-8')]
+# checkin = [json.loads(line) for line in open(files[1], "r")]
+review = [json.loads(line) for line in open(files[2], "r", encoding='utf-8')]
+# user = [json.loads(line) for line in open(files[3], "r")]
 
-    #append all reviews to dictionary of businesses
+#append all reviews to dictionary of businesses
 allbid = {b['business_id']: [] for b in business}
 
 for rev in review:
@@ -30,14 +30,25 @@ en_stop = get_stop_words('en')
 
 p_stemmer = PorterStemmer()
 
-    #Test business to analyze
-testbis = allbid[0]
+#Test business to analyze
+testbis = list(allbid.values())[0:10]
 
 texts = []
-for review in testbis:
-    lowerRev = review['text'].lower()
-    tokens = tokenizer.tokenize(lowerRev)
 
-stopped_tokens = [i for i in tokens if not i in en_stop]
-stemmed_tokens = [p_stemmer.stem(i) for i in stopped_tokens]
-texts.append(stemmed_tokens)
+for reviews in testbis:
+    for review in reviews:
+        lowerRev = review['text'].lower()
+        tokens = lowerRev.split(" ")
+        tokens = [k for k in tokens if k != "" and k != "," and not k.isdigit()]
+        stopped_tokens = [i for i in tokens if not i in en_stop]
+        stemmed_tokens = [p_stemmer.stem(i) for i in stopped_tokens]
+        texts.append(stemmed_tokens)
+# turn our tokenized documents into a id <-> term dictionary
+dictionary = corpora.Dictionary(texts)
+
+# convert tokenized documents into a document-term matrix
+corpus = [dictionary.doc2bow(text) for text in texts]
+
+# generate LDA model
+ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=2, id2word = dictionary, passes=20)
+print(ldamodel.print_topics(num_topics=3, num_words=3)) 
